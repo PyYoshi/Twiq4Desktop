4267bc81b39eb6b8d6571844423d90a5439fb776
PyYoshi <myoshi321go@gmail.com>
2013-04-25 18:33:47 +0900

    add ui_confirm.ui

--- build_resource.sh
+++ build_resource.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pyside-uic Twiq/resources/ui_main.ui -o Twiq/resources/ui_main.py
 pyside-uic Twiq/resources/ui_auth.ui -o Twiq/resources/ui_auth.py
+pyside-uic Twiq/resources/ui_confirm.ui -o Twiq/resources/ui_confirm.py
 pyside-rcc Twiq/resources/resource.qrc -o Twiq/resources/resource_rc.py
