94dbe3b560f46b75da247c87e84d662865c767dc
PyYoshi <myoshi321go@gmail.com>
2013-04-29 18:11:03 +0900

    add ui_del_manage.ui

--- build_resource.sh
+++ build_resource.sh
@@ -2,4 +2,5 @@
 pyside-uic Twiq/resources/ui_main.ui -o Twiq/resources/ui_main.py
 pyside-uic Twiq/resources/ui_auth.ui -o Twiq/resources/ui_auth.py
 pyside-uic Twiq/resources/ui_confirm.ui -o Twiq/resources/ui_confirm.py
+pyside-uic Twiq/resources/ui_del_manage.ui -o Twiq/resources/ui_del_manage.py
 pyside-rcc Twiq/resources/resource.qrc -o Twiq/resources/resource_rc.py
