5d7f49fe75922e0b64e0352f80ff1dd8688c1738
PyYoshi <myoshi321go@gmail.com>
2013-04-29 18:14:33 +0900

    optimize ui

--- Twiq/ui.py
+++ Twiq/ui.py
@@ -10,43 +10,28 @@ from Twiq import _, APP_CONFIG_DIR_PATH
 from Twiq.tw import Twit, Token
 from Twiq.resources.ui_main import Ui_MainWindow
 from Twiq.resources.ui_auth import Ui_AuthorizeWindow
-from Twiq.resources.ui_confirm import Ui_ConfirmDialog
+from Twiq.resources.ui_del_manage import Ui_DelManageDialog
 from Twiq.resources import resource_rc # リソースファイルを読み込む。使用していないわけではない。
 from Twiq.conf import read_app_config, get_accounts_list, \
     read_account_config, AppConfig, AccountConfig, \
     save_account_config, delete_account_config, save_app_config
 
-class ConfirmDialog(QtGui.QMainWindow, Ui_ConfirmDialog):
+class DeleteManageDialog(QtGui.QMainWindow, Ui_DelManageDialog):
     def __init__(self, parent=None):
-        super(ConfirmDialog, self).__init__(parent)
+        super(DeleteManageDialog, self).__init__(parent)
         self.setupUi(self)
+        for k,v in parent.accounts.items():
+            self.accountComboBox.addItem(k)
 
     @QtCore.Slot()
-    def accept(self):
-        pass
-
-    @QtCore.Slot()
-    def reject(self):
-        pass
-
-class DeleteConfirmDialog(ConfirmDialog):
-    def __init__(self, account_name, parent=None):
-        super(DeleteConfirmDialog, self).__init__(parent)
-        self.account_name = account_name
-
-    @QtCore.Slot()
-    def accept(self):
-        try:
-            delete_account_config(self.account_name)
-        except Exception as e:
-            # TODO: エラー出力
-            pass
-        finally:
-            self.close()
-
-    @QtCore.Slot()
-    def reject(self):
-        self.close()
+    def delAccount(self):
+        if self.accountComboBox.currentText():
+            reply = QtGui.QMessageBox.question(self, 'Message',
+                "Are you sure to delete %s?"%self.accountComboBox.currentText(), QtGui.QMessageBox.Yes |
+                QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+            if reply == QtGui.QMessageBox.Yes:
+                delete_account_config(self.accountComboBox.currentText())
+                self.close()
 
 class AuthorizeWindow(QtGui.QMainWindow, Ui_AuthorizeWindow):
     def __init__(self, parent=None):
@@ -85,43 +70,70 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self,parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        self.accounts_group = QtGui.QActionGroup(self)
+        # menuModeを無効化
+        self.menuMode.setEnabled(False)
+        # App Configのロード
         self.app_config = read_app_config()
         if self.app_config == None: self.app_config = AppConfig()
         # APP_CONFIG_DIR_PATHの監視
         self.watcher = QtCore.QFileSystemWatcher([APP_CONFIG_DIR_PATH], parent=self)
         self.connect(self.watcher, QtCore.SIGNAL('directoryChanged(const QString &)'), self.onAppConfDirChanged)
         # 前回使用時のアカウントを選択 & 初期化段階でのアカウント情報の読み込みとcomboboxへの追加
-        last_account_name = self.app_config.selected_account
+        self.accounts = dict()
         self.tw = None
+        last_account_name = self.app_config.selected_account
         self.reloadAccountsConfig()
-        last_account_index = self.accountComboBox.findText(last_account_name)
-        self.accountComboBox.setCurrentIndex(last_account_index)
-        # TODO: 投稿モードプラグインの読み込みとcomboboxへの追加
+        if last_account_name:
+            self.accounts[last_account_name]['action'].setChecked(True)
+            self.tw = self.accounts[last_account_name]['tw']
+        # TODO: 投稿モードプラグインの読み込みとmenuModeへの追加
+
+    def resizeEvent(self, event):
+        # ウィンドウサイズにmsgTextEditのサイズもフィットさせる
+        msgTextEdit_width = self.width()
+        msgTextEdit_height = self.height() - self.menuBar.height()
+        self.msgTextEdit.setGeometry(0,0,msgTextEdit_width,msgTextEdit_height)
+        self.msgCountLabel.setGeometry(msgTextEdit_width-64,msgTextEdit_height-32,self.msgCountLabel.width(),self.msgCountLabel.height())
+        self.msgCountLabel.raise_()
 
     def reloadAccountsConfig(self):
-        cur_account = self.accountComboBox.currentText()
-        self.accountComboBox.clear()
+        # 初期化 & アカウント追加削除アクションの追加
+        self.menuAccount.clear()
+        self.accounts = dict()
+        self.tw = None
+        add_action = QtGui.QAction('Add', self)
+        add_action.setStatusTip('Add Twitter Account')
+        add_action.triggered.connect(self.addAccount)
+        delete_action = QtGui.QAction('Delete', self)
+        delete_action.setStatusTip('Delete Twitter Account')
+        delete_action.triggered.connect(self.delAccount)
+        self.menuAccount.addActions([add_action,delete_action])
+        self.menuAccount.addSeparator()
+        # 選択済みアカウントのチェック
+        checked_account_name = None
+        if self.accounts_group.checkedAction():
+            checked_account_name = self.accounts_group.checkedAction().text()
+        # アカウントをmenuAccountへ追加
+        actions = []
         for account_name in get_accounts_list():
             account_config = read_account_config(account_name)
-            self.addAccountToComboBox(account_config)
-        index = self.accountComboBox.findText(cur_account)
-        if cur_account != None and index != -1:
-            self.accountComboBox.setCurrentIndex(index)
-            self.tw = self.accountComboBox.itemData(self.accountComboBox.currentIndex())['tw']
+            tw = Twit(
+                Token(self.app_config.consumer_key, self.app_config.consumer_secret_key),
+                Token(account_config.access_key, account_config.access_secret_key)
+            )
+            account_action = QtGui.QAction(account_name, self)
+            account_action.setCheckable(True)
+            account_action.setActionGroup(self.accounts_group)
+            actions.append(account_action)
+            self.accounts[account_name] = {'tw':tw,'config':account_config,'action':account_action}
+        self.menuAccount.addActions(actions)
+        if checked_account_name:
+            self.accounts[checked_account_name]['action'].setChecked(True)
+            self.tw = self.accounts[checked_account_name]['tw']
 
     def onAppConfDirChanged(self, path):
         self.reloadAccountsConfig()
-
-    def addAccountToComboBox(self, account_config):
-        tw = Twit(
-            Token(self.app_config.consumer_key, self.app_config.consumer_secret_key),
-            Token(account_config.access_key, account_config.access_secret_key)
-        )
-        account_obj = {
-            'tw':tw,
-            'config':account_config
-        }
-        self.accountComboBox.addItem(account_config.screen_name, account_obj)
 
     @QtCore.Slot()
     def msgTextChanged(self):
@@ -132,29 +144,29 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         self.msgCountLabel.setText(template % v[1])
 
     @QtCore.Slot()
-    def accountStrIndexChanged(self):
-        account = self.accountComboBox.itemData(self.accountComboBox.currentIndex())
-        if account != None: self.tw = account['tw']
-        self.app_config.selected_account = self.accountComboBox.currentText()
-
-    @QtCore.Slot()
-    def modeStrIndexChanged(self):
-        pass
-
-    @QtCore.Slot()
     def addAccount(self):
         self.auth_window = AuthorizeWindow(self)
         self.auth_window.show()
 
     @QtCore.Slot()
     def delAccount(self):
-        account_name = self.accountComboBox.currentText()
-        self.confirm_dialog = DeleteConfirmDialog(account_name,self)
-        self.confirm_dialog.msgLabel.setText('Delete %s account?' % account_name)
-        self.confirm_dialog.show()
+        self.del_manage_dialog = DeleteManageDialog(self)
+        self.del_manage_dialog.show()
+
+    def post_status(self, msg):
+        self.tw.post(msg)
+        self.msgTextEdit.clear()
+
+    def undo_post(self, screen_name):
+        self.msgTextEdit.clear()
+        latest = self.tw.get_user_timeline(screen_name, count=1)[0]
+        self.msgTextEdit.setPlainText(latest.text)
+        self.tw.destroy_status(latest.id)
 
     def keyPressEvent(self, event):
-        screen_name = self.accountComboBox.currentText()
+        screen_name = None
+        if self.accounts_group.checkedAction():
+            screen_name = self.accounts_group.checkedAction().text()
         # Undo
         ctrl_u = event.modifiers() & QtCore.Qt.ControlModifier and (event.key() == QtCore.Qt.Key_U)
         # Post
@@ -163,19 +175,15 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         # TODO: Ctrl+矢印キーでアカウントやモードの切り替え
         msg = self.msgTextEdit.toPlainText()
         v = Twit.validate_msg(msg)
-        if ctrl_enter and v[0]:
+        if ctrl_enter and v[0] and screen_name and self.tw:
             try:
-                self.tw.post(msg)
-                self.msgTextEdit.clear()
+                self.post_status(msg)
             except Exception as e:
                 # TODO: エラー処理
                 pass
-        elif ctrl_u:
+        elif ctrl_u and screen_name and self.tw:
             try:
-                self.msgTextEdit.clear()
-                latest = self.tw.get_user_timeline(screen_name, count=1)[0]
-                self.msgTextEdit.setPlainText(latest.text)
-                self.tw.destroy_status(latest.id)
+                self.undo_post(screen_name)
             except Exception as e:
                 # TODO: エラー処理
                 pass
